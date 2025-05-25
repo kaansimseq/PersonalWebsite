69c0f2cdaa86f5caadccb683465b08656ca037d5
kaansimsek <kaansimsek.dev@gmail.com>
2025-05-25 16:49:51 +0300

    dark mode light mode foto degisikligi

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const body = document.body;
 
 function updateIcon() {
   const icon = document.getElementById("themeIcon");
+  const profilePic = document.querySelector(".profile-pic");
+
   if (body.classList.contains("dark-mode")) {
+    // Dark modda: Güneş ikonunu ve avatar1'i göster
     icon.src = "images/icons/sun-icon.svg";
     icon.alt = "Light Mode";
+    profilePic.src = "images/avatar1.jpg";
   } else {
+    // Light modda: Ay ikonunu ve gözlüklü avatar2'yi göster
     icon.src = "images/icons/moon-icon.svg";
     icon.alt = "Dark Mode";
+    profilePic.src = "images/avatar2.png";
   }
 }
 
